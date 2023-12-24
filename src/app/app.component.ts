702b2f0ee86f841472d84067da6fedd150a37341
gAlex71 <alexvladimirovich71@mail.ru>
2023-12-24 22:21:30 +0300

    base elements angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,19 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: `
+    <h2>{{ title | uppercase }}</h2>
+    <button (click) = 'title = "new title"'>Button</button>
+    <ul>
+      @for (item of newArr; track item) {
+        <li>{{item}}</li>
+      }
+
+      <hr />
+    </ul>
+  `
 })
 export class AppComponent {
   title = 'bot_mini_app';
+  newArr = [1, 2, 3, 4, 5];
 }
